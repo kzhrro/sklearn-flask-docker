c1a0caf536b31c8c52f3ff046d24a0adec96c4a4
kzhrro <66939605+kzhrro@users.noreply.github.com>
2022-05-17 00:11:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-"""
 This file runs the Flask application we are using as an API endpoint.
 """
 
@@ -9,6 +8,35 @@ from flask import request
 from flask import jsonify
 from sklearn import linear_model
 from sklearn.externals import joblib
+import numpy as np
+
+
+class Perceptron:
+    
+    def __init__(self, eta=0.01, n_iter=10):
+        self.eta = eta
+        self.n_iter = n_iter
+    
+    def fit(self, X, y):
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.errors_ = []
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X,y):
+                update=self.eta*(target-self.predict(xi))
+                self.w_[1:] += update *xi
+                self.w_[0] += update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+        return self
+    
+    
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+    
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0, 1, -1)
+
 
 # Create a flask
 app = Flask(__name__)
